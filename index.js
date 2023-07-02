55ae1671a25050a5b672d76339422d93ff9a1ca0
xraxus <xraxus@gmail.com>
2023-07-02 08:47:35 +0200

    Update code according to code review suggestions
    - Improve contrast of generated passwords paragraphs
    - Reformat code
    - Remove unneeded comments & console logs

--- index.js
+++ index.js
@@ -1,62 +1,132 @@
 // Define anchors to html tags
 const generateButton = document.getElementById("generate-btn");
 const numbersCheckbox = document.getElementById("numbers");
-const specialsCheckbox = document.getElementById("specials")
+const specialsCheckbox = document.getElementById("specials");
 
-const lengthInput = document.getElementById("length")
+const lengthInput = document.getElementById("length");
 
-const passwordParagraph1 = document.getElementById("generated-password1")
-const passwordParagraph2 = document.getElementById("generated-password2")
+const passwordParagraph1 = document.getElementById("generated-password1");
+const passwordParagraph2 = document.getElementById("generated-password2");
 
-
-
-// Add event listener to the generate button
-generateButton.addEventListener("click", getPassword)
-
-
+generateButton.addEventListener("click", getPassword);
 
 // Define arrays that hold password characters, numbers and special characters
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-const specials = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const specials = [
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
 
-
-// Function that gets one char from passed array
-function getRandomArrayElement(array){
-    return array[Math.floor(Math.random() * array.length)];
+function getRandomArrayElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
 }
 
-// Function that generates password with said lenght & symbols
-function generatePassword(passSymbols, passLength){
-    let generatedPassword=""
+function generatePassword(passSymbols, passLength) {
+  let generatedPassword = "";
 
-    for (let i = 0; i < passLength; i++){
-        generatedPassword += getRandomArrayElement(passSymbols)
-    }
+  for (let i = 0; i < passLength; i++) {
+    generatedPassword += getRandomArrayElement(passSymbols);
+  }
 
-    console.log(generatedPassword)
-    return generatedPassword
+  return generatedPassword;
 }
 
-// Function that invokes generatePassword() with attibutes chosen by user on the site website & changes two <p> content
-function getPassword(){
-    let passwordSymbols = characters;
+function getPassword() {
+  let passwordSymbols = characters;
 
-    const numbersChecked = numbersCheckbox.checked;
-    const specialsChecked = specialsCheckbox.checked;
-    const passwordLength = lengthInput.value;
+  const numbersChecked = numbersCheckbox.checked;
+  const specialsChecked = specialsCheckbox.checked;
+  const passwordLength = lengthInput.value;
 
-    if (numbersChecked)
-        passwordSymbols = passwordSymbols.concat(numbers)
-    if (specialsChecked)
-        passwordSymbols = passwordSymbols.concat(specials)
+  if (numbersChecked) passwordSymbols = passwordSymbols.concat(numbers);
+  if (specialsChecked) passwordSymbols = passwordSymbols.concat(specials);
 
-    const generatedPassword1 = generatePassword(passwordSymbols, passwordLength)
-    const generatedPassword2 = generatePassword(passwordSymbols, passwordLength)
+  const generatedPassword1 = generatePassword(passwordSymbols, passwordLength);
+  const generatedPassword2 = generatePassword(passwordSymbols, passwordLength);
 
-    passwordParagraph1.textContent = generatedPassword1     
-    passwordParagraph2.textContent = generatedPassword2
+  passwordParagraph1.textContent = generatedPassword1;
+  passwordParagraph2.textContent = generatedPassword2;
 }
-
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-This app allows user to generate a password.
+It's a simple web app that allows to generate a random password.
+
+Check it out here: https://hilarious-jalebi-f9300c.netlify.app/
